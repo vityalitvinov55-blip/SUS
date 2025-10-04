3dd547b86c090949d28b6a2bc42e6c689d1c74f6
ЛитвиновВикторАлександровичИП516 <vita.litvinov.55@gmail.com>
2025-10-04 10:58:06 +0700

    912

--- main.cpp
+++ main.cpp
@@ -2,29 +2,11 @@
 #include <stdlib.h>
 #define SIZE 1024
 int main(int argc, char** argv) {
-<<<<<<< HEAD
-    char* str = gets(str); malloc(sizeof(char) * SIZE);
-    printf("Введите своё имя: \n");
-    scanf("%s", str);
-    str = gets(str);
-    puts("Привет");
-    puts(str);
-=======
-<<<<<<< HEAD
     char* str = (char*)malloc(sizeof(char) * SIZE);
-    printf("Введите своё имя: \n");
+    printf("Введите своё имя:\n");
     scanf("%s", str);
-    puts(" Привет ");
+    puts("Привет");
     puts(str);
     free(str);
     return 0;
-=======
- char* str = (char*)malloc(sizeof(char) * SIZE);
- printf("Введите своё имя: \n");
- scanf("%s", str);
- puts("Привет");
-    puts(str);
- return 0;
->>>>>>> 26b27d5 (Initial commit)
 }
->>>>>>> 4dae59d (Исправлено приветствие)
